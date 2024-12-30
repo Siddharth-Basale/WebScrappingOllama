9ceb1edb48617d386168eb298a88f03c2ad67b36
Siddharth-Basale <132909051+Siddharth-Basale@users.noreply.github.com>
2024-12-30 16:09:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from parse import parse_with_groq
 st.title("The Ultimate Web Scraper 🚀")
 
 # URL Input Field
-url = st.text_input("Enter the URL to scrape (e.g., https://www.swiggy.com):")
+url = st.text_input("Enter the URL to scrape (Enter complete URL, for eg: https://www.swiggy.com):")
 
 # Scrape Button
 if st.button("Scrape"):
@@ -16,8 +16,8 @@ if st.button("Scrape"):
     else:
         st.write("Scraping the URL:", url)
         
+        # Exception Handling for the Scrape Function
         try:
-            # Call the scraping function
             results = scrape(url)
             if results:
                 st.success("Scraping completed successfully!")
@@ -27,19 +27,21 @@ if st.button("Scrape"):
                 st.session_state.dom_content = cleaned_content
 
                 with st.expander("View DOM Content"):
-                    st.text_area("Dom Content", cleaned_content, height=300)
+                    st.text_area("DOM Content", cleaned_content, height=300)
                 
             else:
                 st.warning("No results were returned from the scrape.")
         except Exception as e:
+            # Display error details in Streamlit
             st.error(f"An error occurred: {str(e)}")
-        
+
+# Parse Button (only if content is available)
 if "dom_content" in st.session_state:
-    parse_description = st.text_input("Describe what you would like to pass (e.g., 'Suggest me a product')")
+    parse_description = st.text_input("Describe what you would like to pass (consider - I am from Pune and I want to try something that I won't try regularly, suggest me)")
 
     if st.button("Parse Content"):
         if parse_description:
-            st.write("Parsing the content 🤔")
+            st.write("Parsing the content to ollama 🤔 ")
             dom_chunks = split_dom_content(st.session_state.dom_content)
             results = parse_with_groq(dom_chunks, parse_description)
             st.write(results)
